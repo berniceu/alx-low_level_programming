63a94366c79aa6d4eacf58c86936b38e0acba2d8
berniceu <berniceuiwtuze@gmail.com>
2023-03-18 05:37:21 -0700

    using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: 'alphabet in lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
+
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * Description: 'Print alphabets in lowercase and then uppercase'
+ * Description: 'alphabet in lowercase except q and e'
  * Return: always 0
  */
 int main(void)
 {
 	int n = 97;
-	int m = 65;
 
 	while (n <= 122)
 	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
 		putchar(n);
-		n++;
-	}
-	while (m <= 96)
-	{
-		putchar(m);
 		n++;
 	}
 	putchar('\n');
