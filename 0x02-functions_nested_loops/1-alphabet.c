b0eb24e003b0069a3518d5bc69eb4d4a42cf2cea
berniceu <berniceuiwtuze@gmail.com>
2023-03-21 13:47:31 -0700

    betty

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * description: 'print alphabet in lowercase'
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int n
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
