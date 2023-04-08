23c8646723ad3b08f59bc301daac88c03dc89489
berniceu <berniceuiwtuze@gmail.com>
2023-04-08 02:22:47 -0700

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * description: isupper
+ * return: 1 for uppercase else 0
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
